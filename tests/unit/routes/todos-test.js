e049b56cd1aa2759cc4dad08f1a164a0563d632f
Javier Julio <jjfutbol@gmail.com>
2014-04-11 11:51:10 -0400

    Add first unit route test.

---
+++ tests/unit/routes/todos-test.js
@@ -0,0 +1,41 @@
+import TodosRoute from 'ember-todos/routes/todos';
+
+var route, expectedModel, store;
+
+module('Unit - TodosRoute', {
+  setup: function(){
+    store = { };
+
+    route = TodosRoute.create({
+      store: store
+    });
+  },
+  teardown: function(){
+    Ember.run(route, 'destroy');
+  }
+});
+
+test('it exists', function(){
+  expect(2);
+
+  ok(route);
+  ok(route instanceof Ember.Route);
+});
+
+test('#model', function(){
+  expect(2);
+
+  expectedModel = {
+    id: '1',
+    title: 'install EAK',
+    isCompleted: true
+  };
+
+  store.find = function(type) {
+    equal(type, 'todo');
+
+    return expectedModel;
+  };
+
+  equal(route.model(), expectedModel, 'did not correctly invoke store');
+});
